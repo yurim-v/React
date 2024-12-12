31283ecc337f0ce3a2909146f0bfac532acb6471
yurim-v <urdh96.w@gmail.com>
2024-12-12 23:42:24 +0900

    CRA_24.12.12

---
+++ for-beginner/src/App.js
@@ -0,0 +1,15 @@
+
+
+import Button from "./Button.js"
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className={ styles.title  }>Welcome Back ! </h1>
+      < Button text='Continue ' />
+    </div>
+  );
+}
+
+export default App;
