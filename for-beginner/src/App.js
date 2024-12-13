f5669589722bd867ca61ee269a935d2b5cd072d2
yurim-v <urdh96.w@gmail.com>
2024-12-13 21:04:24 +0900

    useEffect_24.12.13

--- for-beginner/src/App.js
+++ for-beginner/src/App.js
@@ -3,11 +3,54 @@
 import Button from "./Button.js"
 import styles from './App.module.css'
 
+import { useState, useEffect } from 'react'
+
 function App() {
+  const [ count, setCount ] = useState(0);
+  const clickHandler = ( ) => { setCount( current => current + 1)}
+
+  console.log('I run all the time ');
+
+  useEffect(()=>{
+    console.log('I run only Once ');
+  }, [] );
+
+  const [ keyword, setKeyword ] = useState("");
+  const searchHandle = (event)=> {
+    setKeyword( event.target.value );
+  }
+
+  useEffect(()=>{
+
+    if( keyword !== "" ){
+      console.log('I run when "keyword" changes');
+    }
+
+  }, [ keyword ] );
+  
+  useEffect(()=>{
+    console.log('I run when "count" changes');
+  }, [ count ] );
+
+  useEffect(()=>{
+    console.log('I run when "count & keyword" changes');
+  }, [ count , keyword ] );
+
+
+
   return (
     <div>
-      <h1 className={ styles.title  }>Welcome Back ! </h1>
-      < Button text='Continue ' />
+
+      <input type='text' placeholder='Search' 
+        value={ keyword }
+        onChange={ searchHandle } 
+      />
+
+
+      <h1> Count : { count } </h1>
+      < Button 
+        onClick = { clickHandler }
+        text='Click ' />
     </div>
   );
 }
