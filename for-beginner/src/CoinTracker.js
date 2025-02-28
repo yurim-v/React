96b50f81040b7d8f4933a43ea9d88be4d9ee4d34
yurim-v <urdh96.w@gmail.com>
2025-02-28 17:43:44 +0900

    useMemo/option 추가_25.02.28

--- for-beginner/src/CoinTracker.js
+++ for-beginner/src/CoinTracker.js
@@ -1,67 +1,66 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
-function Calculator({ coinName, coinSymbol, coinPrice}){
-  const [ show, setShow] = useState(false);
-  const [ money, setMoney ] = useState(0);
+function Calculator({coinName, coinSymbol, coinPrice}){
+  const [ value, setValue] = useState("");
+  const [show, setShow]  = useState(false);
 
-  const onMoney = (e)=>{
-    setMoney(e.target.value);
+  const onChange = (e)=>{
+    setValue(e.target.value);
     setShow(false)
-  };
-  const onClick = (e)=>{
-    setShow(true);
   }
+  
+  const onClick = (e)=>{setShow(true)}
   return(
     <div>
-      <h3>Coin Calculator</h3>
-      <input onChange={onMoney} value={money} type='number' />
-      <button onClick={onClick}>Calculate</button>
-      { show ? (
-        <div>
-          <p> My Money : {money} (USD)</p>
-          <p> {coinName} : {money / coinPrice} ({coinSymbol})</p>
-        </div>
-        ) : null}
-      
-    </div>
+    <h2> Calculator </h2>
+    <input type='number' placeholder='금액(USD)을 입력하세요.' value={value} onChange={onChange}/>
+    <button onClick={onClick}> calculate </button>
+    { show ? (
+      <div>
+        <p> My Money : {value} (USD)</p>
+        <p> {coinName} : {value} / {coinPrice} ({coinSymbol})</p>
+      </div>
+    ) : null }
+
+  </div>
   )
 }
-
 export default function App(){
-  const [ loading , setLoading ] = useState(true);
+  const [ load , setLoad ] = useState(true);
   const [ coins , setCoins ] = useState([]);
-  const [ value, setValue ] = useState('BTC');
+  const [ option , setOption] = useState('xx');
 
-  const onSelect = (e)=>{
-    setValue(e.target.value); 
-  };
+  const onSelect = (e)=>{setOption(e.target.value)}
 
-  const selectCoin = coins.find((item)=>item.symbol === value);
-  console.log(selectCoin);
+  const selectCoin = useMemo(() => coins.find((item) => item.symbol === option), [option, coins]);
 
   useEffect(()=>{
     fetch('https://api.coinpaprika.com/v1/tickers')
      .then(response => response.json())
-     .then(data => {
-        setLoading(false);
+     .then( data =>{
         setCoins(data);
+        setLoad(false);
      })
   },[])
-
   return(
     <div>
-      <h1> The Coin Tracker ! {loading ? null : `(${coins.length})` }</h1>
-      { loading ? <h2>Loading...</h2> : 
-        <select onChange={onSelect} value={value}>
-          {coins.map(item=>(
-            <option key={item.id} value={item.symbol}> {item.name}({item.symbol}) : {item.quotes.USD.price} USD</option>
-          ))}
-        </select> }
+      <div>
+        <h1> Coin Tracker { load ? null : `(${coins.length})` }  </h1>
+        { load ? <h2>Loading....</h2> :  
+          <select value={option} onChange={onSelect}>
+            <option value='xx'>------------------</option>
+            {coins.map(item=>(
+              <option key={item.id} value={item.symbol}> 
+                {item.name}({item.symbol}) : {item.quotes.USD.price} (USD)
+              </option>
+            ))}
+          </select>
+         }
+      </div>
+      <hr />
 
-        { loading ? null : 
-          <Calculator 
-            coinName={selectCoin.name} coinSymbol={selectCoin.symbol} coinPrice={selectCoin.quotes.USD.price}/>}
+      { load || option ==='xx' ? null : <Calculator coinName={selectCoin.name} coinSymbol={selectCoin.symbol} coinPrice={selectCoin.quotes.USD.price} /> }
+      
     </div>
-
   )
 }
