88d576f8cad589dbf19c44aa4ccb32053ee80b2a
yurim-v <urdh96.w@gmail.com>
2025-05-27 21:21:09 +0900

    react-router-dom_라우터 적용_25.05.27

--- for-beginner/src/MovieApp.js
+++ for-beginner/src/MovieApp.js
@@ -1,41 +1,26 @@
 import { useState, useEffect } from 'react';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Home from './routes/Home.js'
+import Detail from './routes/Detail.js'
+
+
 function App(){
-  const [ loading, setLoading ] = useState(true);
-  const [ movies, setMovies ] = useState([]);
-
-  useEffect(()=>{
-    fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')
-     .then(response => response.json())
-     .then(json =>{
-      console.log(json.data.movies);
-      setMovies(json.data.movies);
-      setLoading(false);
-    })
-  },[])
-
   
 
   return(
-    <div>
-      {loading ? <h1>Loading.... </h1> : 
-        <div>
-          <h1> Movie List ({movies.length})</h1> <hr />
-          {movies.map((movie)=>(
-            <div>
-              <img src={ movie.medium_cover_image}  alt={movie.title + '-image'}/>
-              <h3 key={movie.id}>
-                Title : {movie.title}
-              </h3>
-              <p>  {'< Genres >'} </p>
-              <ul> {movie.genres.map((item)=><li key={item}>{item}</li>)}</ul>
-              <hr />
-            </div>
-          ))}
-        </div>
-      }
-
-    </div>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movie' element={<Detail />} />
+    
+      </Routes>
+    </Router>
   )
 }
 
