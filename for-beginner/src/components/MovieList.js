7c1d07342cc264c2765191a387e250e7efb38f78
yurim-v <urdh96.w@gmail.com>
2025-05-31 11:51:40 +0900

    MovieList_25.05.31

--- for-beginner/src/components/MovieList.js
+++ for-beginner/src/components/MovieList.js
@@ -2,13 +2,13 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const MovieList = ( {coverImage, movieTitle, movieGenres} )=>{
+const MovieList = ( {coverImage, movieTitle, movieGenres, movieId} )=>{
 
   return (
     <div>
       <img src={ coverImage}  alt={movieTitle + '-image'}/>
       <h3 >
-        Title : <Link to='/movie'>{movieTitle}</Link>
+        Title : <Link to={`/movie/${movieId}`}>{movieTitle}</Link>
       </h3>
       <p>  {'< Genres >'} </p>
       <ul> {movieGenres.map((item)=><li key={item}>{item}</li>)}</ul>
@@ -19,6 +19,7 @@ const MovieList = ( {coverImage, movieTitle, movieGenres} )=>{
 }
 
 MovieList.propTypes = {
+  movieId : PropTypes.number.isRequired,
   coverImage :  PropTypes.string.isRequired,
   movieTitle :  PropTypes.string.isRequired,
   movieGenres : PropTypes.arrayOf(PropTypes.string).isRequired
