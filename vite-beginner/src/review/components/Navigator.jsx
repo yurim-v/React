f9de88e0a2f08a06f5d3168affd31ab68de9ab67
KYR <urdhur-study@naver.com>
2025-06-08 20:19:24 +0900

    내비게이션_25.06.08

---
+++ vite-beginner/src/review/components/Navigator.jsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+function Navi(){
+
+  return(
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Review List</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Calculator</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Coin Tracker</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Movie List</a>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navi;
