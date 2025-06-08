59947dc9866879e08bae7edc9a7ffccea2e5cfe7
KYR <urdhur-study@naver.com>
2025-06-08 23:18:58 +0900

    라우터 연결_25.06.08

--- vite-beginner/src/review/components/Navigator.jsx
+++ vite-beginner/src/review/components/Navigator.jsx
@@ -7,7 +7,7 @@ function Navi(){
     <>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Review List</a>
+          <a class="navbar-brand" href="/">Review List</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
