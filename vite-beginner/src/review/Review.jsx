cf13d0c5dea2ce1a00a6c033dedda6acc17b9ceb
KYR <urdhur-study@naver.com>
2025-06-08 23:16:43 +0900

    라우터 추가_25.06.08

--- vite-beginner/src/review/Review.jsx
+++ vite-beginner/src/review/Review.jsx
@@ -1,10 +1,27 @@
-import Navi from './components/Navigator.jsx'
+import Navi from './components/Navigator.jsx';
+import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+
+import Home from './Router/home.jsx';
+import CoinTracker from './Router/coinTracker.jsx';
+import Calculator from './Router/calculator.jsx';
+import MovieList from './Router/MovieList.jsx';
+
 
 function App(){
 
   return(
     <> 
       <Navi />
+      <Router>
+        <Routes>
+          <Route path='/' element={< Home />} />
+          <Route path='/coinTracker' element={<CoinTracker />}/>
+          <Route path='calculator' element={<Calculator />}/>
+          <Route path='/movieList' element={<MovieList /> }/>
+
+
+        </Routes>
+      </Router>
     </>
   )
 
