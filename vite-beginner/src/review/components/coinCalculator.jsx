d5f5c03ce45514fd655830a10c683e2a5bcda7d1
KYR <urdhur-study@naver.com>
2025-06-11 06:11:14 +0900

    Coin Calculator 작성중..._25.06.11

---
+++ vite-beginner/src/review/components/coinCalculator.jsx
@@ -0,0 +1,17 @@
+
+
+function Calculator({ coinName, coinSymbol, coinPrice}){
+
+  return(
+    <>
+      <div>
+        <hr />
+        <h3>Coin Calculator</h3>
+
+
+      </div>
+    </>
+  )
+}
+
+export default Calculator;
