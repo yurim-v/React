02663fe89c756e580e6c5d867eb2bbcd1cba308b
KYR <urdhur-study@naver.com>
2025-06-11 06:10:43 +0900

    Coin Tracker..작성중_25.06.11

--- vite-beginner/src/review/Router/coinTracker.jsx
+++ vite-beginner/src/review/Router/coinTracker.jsx
@@ -1,12 +1,46 @@
-
-
+import { useState, useEffect } from 'react';
+import Calculator from '../components/coinCalculator.jsx'
 
 function CoinTracker(){
+  const [ loading, setLoading ] = useState(true);
+  const [ coins, setCoins ] = useState([]);
+  const [ option, setOption ] = useState('xx');
+  const [ coin, setCoin ] = useState({});
 
+  async function getCoins(){
+    let data = await (await fetch(' https://api.coinpaprika.com/v1/tickers?limit=10')).json();
+    setLoading(false)
+    setCoins(data);
+  }
+
+  const changeHanlder = (event)=>{
+    setCoin(coins.find(item =>(item.name === event.target.value)))
+    setOption(event.target.value);
+  }
+
+  useEffect(()=>{
+    getCoins();
+  },[])
+
+    useEffect(()=>{
+      console.log(coin)
+    },[coin])
+  
+  
   return(
     <>
-      <h1>CoinTracker</h1>
-
+      <h1>CoinTracker {loading ? null : `(${coins.length})`}</h1>
+      <div>
+        { loading ? <h1>Loading...</h1>: 
+          <select value={option} onChange={changeHanlder}>
+            <option value='xx'>-------------------------</option>
+            {coins.map(coin => (
+              <option key={coin.id} value={coin.name}>{coin.name}({coin.symbol}) : {coin.quotes.USD.price}(USD)</option>
+            ))}
+          </select>
+        }
+         { option =='xx' ? null : < Calculator coinName={coin.name} coinSymbol={coin.symbol} coinPrice={coin.quotes.USD.price} />}
+      </div>
     </>
   )
 }
