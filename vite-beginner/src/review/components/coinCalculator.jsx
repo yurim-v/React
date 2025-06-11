fcae0c82d5c8ea635e4b62a6ce8edf754cb2e823
KYR <urdhur-study@naver.com>
2025-06-11 20:18:22 +0900

    Coin Calculator_25.06.11

--- vite-beginner/src/review/components/coinCalculator.jsx
+++ vite-beginner/src/review/components/coinCalculator.jsx
@@ -1,6 +1,9 @@
 
+import {useState} from 'react'
 
 function Calculator({ coinName, coinSymbol, coinPrice}){
+  const [ calculate, setCalculate ] = useState(false);
+  const [ money, setMoney ] = useState(0);
 
   return(
     <>
@@ -8,7 +11,18 @@ function Calculator({ coinName, coinSymbol, coinPrice}){
         <hr />
         <h3>Coin Calculator</h3>
 
-
+        <label> My Money (USD) : 
+          <input value={money} onChange={(e)=>{
+            setMoney(e.target.value)
+            setCalculate(false)  ;
+          }}/>
+        </label>
+        <button onClick={()=>{{
+          setCalculate(true);
+        }}}>Calculate</button>
+        { calculate ? <p>
+          {coinName} ({coinSymbol}) : {money} / {coinPrice} 
+        </p> : null}
       </div>
     </>
   )
